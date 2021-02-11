a378e707b8073cf96139cde8df0b057f1a01eb7c
MSpilari <mb.spilari@hotmail.com>
2021-02-11 17:38:44 -0300

    Iniciando a página de Skills

--- pages/skills.js
+++ pages/skills.js
@@ -1,7 +1,26 @@
-import Header from '../src/components/Header'
-
+import DefaultPage from '../src/components/DefaultPage'
+import PageTitle from '../src/components/Titles'
 const Skills = () => {
-	return <Header></Header>
+	return (
+		<DefaultPage>
+			<div>
+				<PageTitle text={'Habilidades & Experiência'} />
+				<p>
+					Possuo mais conhecimento com o FrontEnd.
+					<br />
+					<br />
+					HTML, CSS, SASS, Styled Components, Javascript, Typescript, Node,
+					Next, Python.
+					<br />
+					Construindo pequenas e médias aplicações para a Web principalmente com
+					React, utilizando também animações, layouts interativos com o conceito
+					Mobile first e Single Page Application(SPA).
+					<br />
+					<br />
+				</p>
+			</div>
+		</DefaultPage>
+	)
 }
 
 export default Skills
