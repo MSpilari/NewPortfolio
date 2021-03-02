95f32e88e8e955ff51593d87664f396dd69594da
MSpilari <mb.spilari@hotmail.com>
2021-03-02 12:12:58 -0300

    Resolvendo os problemas de Css

--- src/components/pages/Skills/styles.js
+++ src/components/pages/Skills/styles.js
@@ -4,6 +4,9 @@ const InfoDiv = styled.div`
 	@media screen and (max-width: 768px) {
 		width: 100%;
 	}
+	@media screen and (min-width: 482px) {
+		display: flex;
+	}
 `
 const Info = styled.p`
 	color: ${({ theme }) => theme.colors.text};
