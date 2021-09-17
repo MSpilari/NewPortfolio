86cb6e6f1dc51c2ae94b55bce066108482eff55b
MSpilari <mb.spilari@hotmail.com>
2021-09-17 12:25:24 -0300

    Separando o estilo de Titles

--- src/components/Titles/index.jsx
+++ src/components/Titles/index.jsx
@@ -1,18 +1,4 @@
-import styled from 'styled-components'
-
-const FontStyled = styled.span`
-	font-size: 50px;
-	font-weight: 900;
-	color: ${({ theme }) => theme.colors.text};
-	padding: 1px;
-	cursor: pointer;
-
-	transition: all 0.5s linear;
-
-	&:hover {
-		color: ${({ theme }) => theme.colors.hoverButtons};
-	}
-`
+import { FontStyled } from './styles'
 
 const CustomCharacters = ({ text }) => {
 	const customizedChars = text.split('').map((char, index) => {
