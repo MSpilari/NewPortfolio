c9585ea97c3bec5d56aa1aaad0a8dbc0a7565700
MSpilari <mb.spilari@hotmail.com>
2021-09-17 12:29:55 -0300

    Separando os estilos do SocialIcons

--- src/components/Header/SocialIcons/index.jsx
+++ src/components/Header/SocialIcons/index.jsx
@@ -1,18 +1,4 @@
-import styled from 'styled-components'
-
-const StyledSocialIcon = styled.i`
-	color: ${({ theme }) => theme.colors.text};
-	margin: 5px 0;
-	font-size: 20px;
-
-	&:hover {
-		color: ${({ theme }) => theme.colors.hoverButtons};
-	}
-
-	@media screen and (max-width: 768px) {
-		margin: 0 5px;
-	}
-`
+import { StyledSocialIcon } from './styles'
 
 const SocialIcon = ({ href, children }) => {
 	return (
