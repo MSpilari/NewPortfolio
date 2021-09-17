0335e90dec1a610526d1b062fb9665516743d3f7
MSpilari <mb.spilari@hotmail.com>
2021-09-17 12:09:39 -0300

    Separando o estilo do botão

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,21 +1,4 @@
-import styled from 'styled-components'
-
-const StyledButton = styled.button`
-	background-color: transparent;
-	border: 3px solid ${({ theme }) => theme.colors.text};
-	padding: 20px 10px;
-	font-size: 18px;
-	font-weight: 600;
-	color: ${({ theme }) => theme.colors.text};
-	cursor: pointer;
-
-	margin: 20px 0;
-
-	&:hover {
-		border: 3px solid ${({ theme }) => theme.colors.hoverButtons};
-		background-color: ${({ theme }) => theme.colors.hoverButtons};
-	}
-`
+import { StyledButton } from './style'
 
 const Button = ({ label, ...props }) => {
 	return <StyledButton {...props}>{label}</StyledButton>
---
+++ src/components/Button/style.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+const StyledButton = styled.button`
+	background-color: transparent;
+	border: 3px solid ${({ theme }) => theme.colors.text};
+	padding: 20px 10px;
+	font-size: 18px;
+	font-weight: 600;
+	color: ${({ theme }) => theme.colors.text};
+	cursor: pointer;
+
+	margin: 20px 0;
+
+	&:hover {
+		border: 3px solid ${({ theme }) => theme.colors.hoverButtons};
+		background-color: ${({ theme }) => theme.colors.hoverButtons};
+	}
+`
+
+export { StyledButton }
