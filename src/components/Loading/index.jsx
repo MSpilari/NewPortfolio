eb5a856fa77baa235f9321f417f7e8bba70b2e51
MSpilari <mb.spilari@hotmail.com>
2021-09-17 12:23:09 -0300

    Separando o estilo do Loading

--- src/components/Loading/index.jsx
+++ src/components/Loading/index.jsx
@@ -1,29 +1,5 @@
-import styled from 'styled-components'
+import { LoadingWrapper, Rotate } from './styles'
 
-const LoadingWrapper = styled.div`
-	width: 100%;
-	height: 200px;
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	justify-content: center;
-`
-const Rotate = styled.div`
-	width: 50px;
-	height: 75px;
-	border-top: solid 5px ${({ theme }) => theme.colors.hoverButtons};
-	border-radius: 100px;
-	animation: rotate 2s linear infinite;
-	margin-bottom: 5px;
-	@keyframes rotate {
-		from {
-			transform: rotate(0deg);
-		}
-		to {
-			transform: rotate(360deg);
-		}
-	}
-`
 const Loading = () => {
 	return (
 		<LoadingWrapper>
