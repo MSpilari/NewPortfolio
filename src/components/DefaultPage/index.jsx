d6153c884b84a598d3408556365c0d80c1e9f028
MSpilari <mb.spilari@hotmail.com>
2021-09-17 12:20:27 -0300

    Separando o estilo na Default

--- src/components/DefaultPage/index.jsx
+++ src/components/DefaultPage/index.jsx
@@ -1,54 +1,6 @@
-import styled from 'styled-components'
-
 import NavBar from '../Header'
+import { MainWrapper, FirstChildWrapper, SecondChildWrapper } from './styles'
 
-const MainWrapper = styled.main`
-	width: 92%;
-	height: 100%;
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	align-self: flex-end;
-	flex-wrap: wrap;
-
-	@media screen and (max-width: 768px) {
-		align-self: center;
-		height: 92%;
-		width: 100%;
-	}
-`
-
-const FirstChildWrapper = styled.div`
-	width: ${() =>
-		typeof window !== 'undefined' && document.location.pathname === '/projects'
-			? '100%'
-			: '50%'};
-	display: flex;
-	align-items: center;
-	justify-content: center;
-
-	@media screen and (max-width: 768px) {
-		width: 95%;
-		margin: 0 auto;
-	}
-`
-const SecondChildWrapper = styled.div`
-	width: 50%;
-	display: ${() =>
-		typeof window !== 'undefined' && document.location.pathname === '/projects'
-			? 'none'
-			: 'flex'};
-	align-items: center;
-	justify-content: center;
-
-	@media screen and (max-width: 768px) {
-		display: ${() =>
-			typeof window !== 'undefined' && document.location.pathname === '/skills'
-				? 'flex'
-				: 'none'};
-		width: 95%;
-	}
-`
 const isClientSide = () => typeof window !== 'undefined'
 
 const DefaultPage = ({ firstChild, secondChild }) => {
