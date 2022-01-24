6c0175137fa80552bead08c8d689133245d6273c
MSpilari <mb.spilari@hotmail.com>
2022-01-24 16:40:04 -0300

    Criando os hooks referentes ao uso do contexto de linguagem e autorização

---
+++ src/hooks/authenticationHook.ts
@@ -0,0 +1,6 @@
+import { useContext } from 'react'
+import { AuthenticationContext } from '../context/authentication'
+
+const useAuthentication = () => useContext(AuthenticationContext)
+
+export { useAuthentication }
---
+++ src/hooks/languageHook.ts
@@ -0,0 +1,6 @@
+import { useContext } from 'react'
+import { LanguageContext } from '../context/language'
+
+const useLanguage = () => useContext(LanguageContext)
+
+export { useLanguage }
