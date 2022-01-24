d142feabad93ea8db2c33b6b4db29d6319669db3
MSpilari <mb.spilari@hotmail.com>
2022-01-24 16:52:37 -0300

    Criando a animação da página sobre para desktop

---
+++ src/components/UI/About/AboutAnimationDesktop.tsx
@@ -0,0 +1,17 @@
+import Lottie from 'react-lottie-player'
+import AboutAnimationLarge from '../../../assets/AboutAnimation.json'
+
+const AboutAnimationDesktop = () => {
+	return (
+		<div className=' hidden w-full h-full overflow-hidden absolute lg:flex'>
+			<Lottie
+				loop
+				play
+				className='w-full'
+				animationData={AboutAnimationLarge}
+			/>
+		</div>
+	)
+}
+
+export { AboutAnimationDesktop }
