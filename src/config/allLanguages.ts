928b27e019f5bee49c42b8f7549580c542fc432e
MSpilari <mb.spilari@hotmail.com>
2022-01-24 16:38:10 -0300

    Adicionando os arquivos de linguagens e do logos das tecnologias

---
+++ src/config/allLanguages.ts
@@ -0,0 +1,73 @@
+const allLanguages = {
+	ptBr: {
+		Hello1: 'Olá, ',
+		Hello2: ' eu sou o',
+		Name: ' Matheus B. Spilari',
+		Role: 'Fullstack Developer',
+		HomeTitle: 'Olá, eu sou o Matheus, Web Developer.',
+		ButtonLabel: 'Entrar em contato !',
+		ButtonMessageLabel: 'Enviar Mensagem !',
+		AboutTitle: 'Sobre Mim',
+		AboutInfo: `Amante da tecnologia, sempre buscando conhecer as tecnologias que estão em alta no
+					mercado, mais utilizadas pelas grandes empresas do ramo.
+					
+					Uma pessoa muito organizada, pragmática, resolutor de problemas,
+					autodidata que gosta de se atentar aos detalhes. Apaixonado por
+					videogames, por isso conheci a programação. Sou fã de computação, desde
+					pequeno desmontava alguns computadores. Hoje monto os meus para o
+					trabalho e para minha diversão.
+					
+					Curioso por todas as tecnologias/linguagens, mas atualmente trabalho com
+					Javascript no Frontend e no Backend.`,
+		SkillsTitle: 'Habilidades & Experiência',
+		SkillsInfo: `Possuo conhecimento tanto no Frontend como no Backend.
+					HTML, CSS, CSS Modules, SASS, Styled Components, TailwindCss, Javascript, Typescript, Node, Next,
+					Python, Google Cloud Platform.
+					Trabalhando como freelancer, sozinho ou em squads.
+					Construindo pequenas, médias e grandes aplicações para a Web, principalmente com
+					React, Kanban, utilizando também animações, layouts interativos com o conceito
+					Mobile first, Single Page Application(SPA) e Design Responsivo.
+					Também possuo uma experiência no Backend, com NodeJs, construindo APIs no padrão
+					RESTful/REST, utilizando Express, autenticação JWT, OAuth2.0, DBs como PostgreSQL, MongoDB, Firebase.
+					Visite meu Linkedin para entrar em contato.`,
+		ProjectsTitle: 'Projetos',
+		ContactTitle: 'Me Contate !'
+	},
+	en: {
+		Hello1: 'Hello, ',
+		Hello2: ' i´m',
+		Name: ' Matheus B. Spilari',
+		Role: 'Fullstack Developer',
+		HomeTitle: 'Hello, I´m Matheus, Web Developer.',
+		ButtonLabel: 'Contact Me !',
+		ButtonMessageLabel: 'Send a Message !',
+		AboutTitle: 'About Me',
+		AboutInfo: `Technology lover, always seeking to know the technologies that are on the rise in the
+			market, most used by large companies in the field.
+			
+			A very organized, pragmatic, problem-solving person,
+			self-taught who likes to pay attention to details. In love with
+			video games, so I got to know programming. I've been a computer fan since
+			small dismantled some computers. Today I assemble mine for the
+			work and for my fun.
+			
+			Curious about all technologies/languages, but I currently work with
+			Javascript on Frontend and Backend.
+		`,
+		SkillsTitle: 'Skills & Experience',
+		SkillsInfo: `I have knowledge in both Frontend and Backend.
+			HTML, CSS, CSS Modules, SASS, Styled Components, TailwindCss, Javascript, Typescript, Node, Next,
+			Python, Google Cloud Platform.
+			Working as a freelancer, alone or in squads.
+			Building small, medium and large applications for the Web, mainly with
+			React, Kanban, also using animations, interactive layouts with the concept
+			Mobile first, Single Page Application(SPA) and Responsive Design.
+			I also have an experience in the Backend, with NodeJs, building APIs in the pattern
+			RESTful/REST, using Express, JWT authentication, OAuth2.0, DBs like PostgreSQL, MongoDB, Firebase.
+			Visit my Linkedin to get in touch.`,
+		ProjectsTitle: 'Projects',
+		ContactTitle: 'Contact Me !'
+	}
+}
+
+export { allLanguages }
