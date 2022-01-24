b1e8a25f8b472fa6920b940497af86bf097c0738
MSpilari <mb.spilari@hotmail.com>
2022-01-24 16:54:13 -0300

    Criando o texto da página sobre

---
+++ src/components/UI/About/AboutText.tsx
@@ -0,0 +1,26 @@
+import { useLanguage } from '../../../hooks/languageHook'
+
+const AboutText = () => {
+	const { language } = useLanguage()
+	return (
+		<div
+			className='w-full h-full mx-auto absolute z-10 text-white 
+    flex flex-col items-center justify-around lg:w-[400px] lg:h-[500px] lg:bottom-0'
+		>
+			<h1
+				className='font-pushster font-bold text-3xl animate-bounce
+       text-yellow-500'
+			>
+				{language.AboutTitle}
+			</h1>
+			<p
+				className='font-pushster text-lg text-center mx-2 
+      sm:text-xl sm:mt-32 lg:mt-0 lg:ml-4'
+			>
+				{language.AboutInfo}
+			</p>
+		</div>
+	)
+}
+
+export { AboutText }
