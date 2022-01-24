816b60a4bc560fe7a9fe49054af0f9f92b91df3f
MSpilari <mb.spilari@hotmail.com>
2022-01-24 17:02:55 -0300

    Criando a página de projetos com seus componentes

---
+++ pages/projects.tsx
@@ -0,0 +1,19 @@
+import { Header } from '../src/components/UI/Header'
+import { ProjectAnimationDesktop } from '../src/components/UI/Projects/projectAnimationDesktop'
+import { ProjectCarousel } from '../src/components/UI/Projects/ProjectCarousel'
+
+const Projects = () => {
+	return (
+		<div className='w-screen h-screen lg:flex'>
+			<Header />
+
+			<div className='flex flex-col h-[calc(100%-128px)] relative lg:flex-row lg:w-[calc(100%-80px)] lg:h-full '>
+				<ProjectAnimationDesktop />
+
+				<ProjectCarousel />
+			</div>
+		</div>
+	)
+}
+
+export default Projects
