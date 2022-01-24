82593dc5b5b6c365dafa9c025a474029766f5852
MSpilari <mb.spilari@hotmail.com>
2022-01-24 17:02:14 -0300

    Criando a animação dos projetos para desktop

---
+++ src/components/UI/Projects/projectAnimationDesktop.tsx
@@ -0,0 +1,17 @@
+import Lottie from 'react-lottie-player'
+import projectDesktopAnimation from '../../../assets/projectAnimationDesktop.json'
+
+const ProjectAnimationDesktop = () => {
+	return (
+		<div className=' hidden w-full h-full overflow-hidden absolute lg:flex'>
+			<Lottie
+				loop
+				play
+				className='w-full'
+				animationData={projectDesktopAnimation}
+			/>
+		</div>
+	)
+}
+
+export { ProjectAnimationDesktop }
