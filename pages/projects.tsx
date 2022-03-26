4dddda26a670e5b1cf3bc18cd59d495fd63c195e
MSpilari <mb.spilari@hotmail.com>
2022-03-26 13:41:25 -0300

    Colocando o getServerSideProps para que os dados, vindos do DB, sejam inseridos na tela com maior rapidez

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -2,7 +2,7 @@ import { Header } from '../src/components/UI/Header'
 import { ProjectAnimationDesktop } from '../src/components/Projects/projectAnimationDesktop'
 import { ProjectCarousel } from '../src/components/Projects/ProjectCarousel'
 
-const Projects = () => {
+const Projects = ({ data }) => {
 	return (
 		<div className='w-screen h-screen lg:flex'>
 			<Header />
@@ -10,10 +10,17 @@ const Projects = () => {
 			<div className='flex flex-col h-[calc(100%-128px)] relative lg:flex-row lg:w-[calc(100%-80px)] lg:h-full '>
 				<ProjectAnimationDesktop />
 
-				<ProjectCarousel />
+				<ProjectCarousel projectsInfo={data} />
 			</div>
 		</div>
 	)
+}
+
+export async function getServerSideProps() {
+	const res = await fetch(`${process.env.CURRENT_URL}/api/allProjects`)
+	const data = await res.json()
+
+	return { props: { data } }
 }
 
 export default Projects
