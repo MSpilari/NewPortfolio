1bf40d04078a96127815d07865901717eeda1bb8
MSpilari <mb.spilari@hotmail.com>
2022-06-02 12:56:42 -0300

    Colocando a tag de header no organismo Header

--- src/components/03_Organisms/Header/index.tsx
+++ src/components/03_Organisms/Header/index.tsx
@@ -4,18 +4,23 @@ import { NavigationBar } from '../../02_Molecules/NavigationBar'
 
 const Header = () => {
 	return (
-		<section className='flex flex-col items-center w-full h-full'>
-			<div
-				className='w-full h-full flex items-center justify-center 
+		<header
+			className='bg-zinc-900 w-full h-20 border-b-2 border-yellow-400 font-pushster 
+										lg:h-screen lg:w-20 lg:border-r-2 lg:border-b-0'
+		>
+			<section className='flex flex-col items-center w-full h-full'>
+				<div
+					className='w-full h-full flex items-center justify-center 
 											lg:w-20 lg:h-56 lg:flex-col xl:h-2/3'
-			>
-				<Logo />
+				>
+					<Logo />
 
-				<LinksUnorderedList />
-			</div>
+					<LinksUnorderedList />
+				</div>
 
-			<NavigationBar />
-		</section>
+				<NavigationBar />
+			</section>
+		</header>
 	)
 }
 
