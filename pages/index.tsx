5e9544e5e18d729226d2bf56236098d5e72623ec
MSpilari <mb.spilari@hotmail.com>
2022-06-02 17:45:21 -0300

    Inserindo o AnimationWrapper com o Animation com CSS único de cada page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import AnimationDesktopFile from '../src/assets/animationDesktop1.json'
 import AnimationMobileFile from '../src/assets/animationMobile.json'
 
-import { Animation } from '../src/components/03_Organisms/Animation'
+import { Animation } from '../src/components/01_Atoms/Animation'
 import { Header } from '../src/components/03_Organisms/Header'
 import { Greet } from '../src/components/03_Organisms/Greet'
+import { AnimationWrapper } from '../src/components/04_Templates/AnimationWrapper'
 
 const HomePage = () => {
 	return (
@@ -17,10 +18,21 @@ const HomePage = () => {
 				className='flex flex-col h-[calc(100%-128px)] relative 
 												lg:flex-row lg:w-[calc(100%-80px)] lg:h-full '
 			>
-				<Animation
-					animationFileMobile={AnimationMobileFile}
-					animationFileDesktop={AnimationDesktopFile}
-				/>
+				<AnimationWrapper
+					className='w-full overflow-hidden absolute top-[15%] left-[-30px] max-w-3xl 
+																	sm:left-0 sm:top-0 
+																	lg:left-0 lg:top-0 lg:max-w-none lg:h-full'
+				>
+					<Animation
+						animationFileMobile={AnimationMobileFile}
+						animationFileDesktop={AnimationDesktopFile}
+						className='w-[400px] 
+												xxs:w-[450px] 
+												xs:w-[500px] 
+												sm:w-[calc(100%-64px)]
+												lg:w-full'
+					/>
+				</AnimationWrapper>
 
 				<Greet />
 			</div>
