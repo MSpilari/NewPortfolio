dd2175a5cb5dd1051cb5133d0d52d3200bac6e92
MSpilari <mb.spilari@hotmail.com>
2022-06-02 13:01:56 -0300

    Inserindo o Header e a Animation vindos de Organisms

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,26 @@
-import { Header } from '../src/components/UI/Header'
-import { AnimationDesktop } from '../src/components/Home/AnimationDesktop'
-import { AnimationMobile } from '../src/components/Home/AnimationMobile'
+import AnimationDesktopFile from '../src/assets/animationDesktop1.json'
+import AnimationMobileFile from '../src/assets/animationMobile.json'
+
+import { Animation } from '../src/components/03_Organisms/Animation'
+import { Header } from '../src/components/03_Organisms/Header'
 import { Greet } from '../src/components/Home/Greet'
 
 const HomePage = () => {
 	return (
-		<div className='w-screen h-screen lg:flex'>
+		<div
+			className='w-screen h-screen 
+											lg:flex'
+		>
 			<Header />
 
-			<div className='flex flex-col h-[calc(100%-128px)] relative lg:flex-row lg:w-[calc(100%-80px)] lg:h-full '>
-				<AnimationMobile />
-				<AnimationDesktop />
-
+			<div
+				className='flex flex-col h-[calc(100%-128px)] relative 
+												lg:flex-row lg:w-[calc(100%-80px)] lg:h-full '
+			>
+				<Animation
+					animationFileMobile={AnimationMobileFile}
+					animationFileDesktop={AnimationDesktopFile}
+				/>
 				<Greet />
 			</div>
 		</div>
