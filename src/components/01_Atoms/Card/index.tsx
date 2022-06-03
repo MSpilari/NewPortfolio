2566aee9f0ae049e5e933f80a960bda09e7501b6
MSpilari <mb.spilari@hotmail.com>
2022-06-03 12:27:04 -0300

    Passando Card para Atom

---
+++ src/components/01_Atoms/Card/index.tsx
@@ -0,0 +1,24 @@
+interface ICard {
+	icon: JSX.Element
+	name: string
+}
+
+const Card: React.FC<ICard> = ({ icon, name }) => {
+	return (
+		<div className='flex items-center mx-auto justify-center w-full h-full'>
+			<div className='flex flex-col items-center w-full h-full relative animateSkill'>
+				<div className='border-2 border-white'>
+					<i className='text-5xl'>{icon}</i>
+				</div>
+
+				<div className='flex items-center justify-center h-full absolute top-0 opacity-0'>
+					<p className='text-xl text-center font-pushster text-yellow-500'>
+						{name}
+					</p>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export { Card }
