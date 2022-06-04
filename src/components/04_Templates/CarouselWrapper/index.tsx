dff63674a34736203c815e890bde837a4cd2083b
MSpilari <mb.spilari@hotmail.com>
2022-06-04 14:01:04 -0300

    Criando o CarouselWrapper

---
+++ src/components/04_Templates/CarouselWrapper/index.tsx
@@ -0,0 +1,16 @@
+import { useLanguage } from '../../../hooks/languageHook'
+
+const CarouselWrapper: React.FC = ({ children }) => {
+	const { language } = useLanguage()
+
+	return (
+		<div className='w-full h-full mx-auto absolute z-10 text-white flex flex-col items-center justify-center'>
+			<h1 className='font-pushster font-bold text-3xl animate-bounce text-yellow-500'>
+				{language.ProjectsTitle}
+			</h1>
+			{children}
+		</div>
+	)
+}
+
+export { CarouselWrapper }
