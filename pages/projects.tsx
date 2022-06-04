3284a0f498834b48fef6caacae9f1f2b6e49d763
MSpilari <mb.spilari@hotmail.com>
2022-06-04 14:04:44 -0300

    Inserindo a AnimationWrapper, Animation e o ProjectCarousel vindo de Organisms

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,14 +1,28 @@
-import { Header } from '../src/components/UI/Header'
-import { ProjectAnimationDesktop } from '../src/components/Projects/projectAnimationDesktop'
-import { ProjectCarousel } from '../src/components/Projects/ProjectCarousel'
+import { Animation } from '../src/components/01_Atoms/Animation'
+import { Header } from '../src/components/03_Organisms/Header'
+import { ProjectCarousel } from '../src/components/03_Organisms/ProjectCarousel'
+import { AnimationWrapper } from '../src/components/04_Templates/AnimationWrapper'
+
+import ProjectDesktopAnimationFile from '../src/assets/projectAnimationDesktop.json'
 
 const Projects = ({ data }) => {
 	return (
-		<div className='w-screen h-screen lg:flex'>
+		<div
+			className='w-screen h-screen 
+										lg:flex'
+		>
 			<Header />
 
-			<div className='flex flex-col h-[calc(100%-128px)] relative lg:flex-row lg:w-[calc(100%-80px)] lg:h-full '>
-				<ProjectAnimationDesktop />
+			<div
+				className='flex flex-col h-[calc(100%-128px)] relative 
+											lg:flex-row lg:w-[calc(100%-80px)] lg:h-full '
+			>
+				<AnimationWrapper className='w-full h-full overflow-hidden absolute'>
+					<Animation
+						className='w-full'
+						animationFileDesktop={ProjectDesktopAnimationFile}
+					/>
+				</AnimationWrapper>
 
 				<ProjectCarousel projectsInfo={data} />
 			</div>
