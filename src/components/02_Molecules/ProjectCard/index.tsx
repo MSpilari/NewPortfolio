135eeba7a875c5ca2ea169929b9a51c27f00b4ea
MSpilari <mb.spilari@hotmail.com>
2022-06-06 12:34:23 -0300

    Inserindo a tag Image, estabelecendo uma altura fixa para as imagens e trocando os botões pelo átomo Button

--- src/components/02_Molecules/ProjectCard/index.tsx
+++ src/components/02_Molecules/ProjectCard/index.tsx
@@ -1,4 +1,7 @@
 import { BsGithub, BsEyeFill } from 'react-icons/bs'
+import Image from 'next/image'
+
+import { Button } from '../../01_Atoms/Button'
 
 interface IProject {
 	image: string
@@ -10,36 +13,38 @@ interface IProject {
 const ProjectCard = ({ image, prjLink, title, gitLink }: IProject) => {
 	return (
 		<div
-			className='flex flex-col font-pushster w-[200px] h-full mx-3 bg-[rgba(0,0,0,0.5)] rounded-md 
+			className='flex flex-col font-pushster w-[200px] h-[50vh] mx-3 bg-[rgba(0,0,0,0.5)] rounded-md 
                   lg:w-[350px] lg:h-[350px]'
 		>
-			<img
-				className='w-full p-2 rounded-xl'
-				src={image}
-				alt={`${title} image`}
-			/>
+			<div className='relative w-full h-3/4 px-2 rounded-md overflow-hidden'>
+				<Image
+					layout='fill'
+					objectFit='cover'
+					sizes='50vw'
+					src={image}
+					alt={`${title} image`}
+				/>
+			</div>
 
-			<h1 className='w-full text-xl text-yellow-500 truncate'>{title}</h1>
+			<h1 className='w-full text-xl text-yellow-500 ml-2 truncate'>{title}</h1>
 
 			<div className='w-full flex justify-between my-2 '>
 				<a href={gitLink} target='_blank' rel='noreferrer'>
-					<button
-						className='border-2 flex items-center rounded-xl 
-          text-base px-4 py-2 hover:text-yellow-500 hover:border-yellow-500'
-					>
-						<BsGithub className='mr-1' />
-						Code
-					</button>
+					<Button
+						className='border-2 flex items-center rounded-xl text-base px-4 py-2 
+											hover:text-yellow-500 hover:border-yellow-500'
+						iconLabel={<BsGithub className='mr-1' />}
+						animationText='Code'
+					/>
 				</a>
 
 				<a href={prjLink} target='_blank' rel='noreferrer'>
-					<button
-						className='border-2 flex items-center rounded-xl 
-          text-base px-4 py-2 hover:text-yellow-500 hover:border-yellow-500'
-					>
-						<BsEyeFill className='mr-1' />
-						Project
-					</button>
+					<Button
+						className='border-2 flex items-center rounded-xl text-base px-4 py-2 
+											hover:text-yellow-500 hover:border-yellow-500'
+						iconLabel={<BsEyeFill className='mr-1' />}
+						animationText='Project'
+					/>
 				</a>
 			</div>
 		</div>
