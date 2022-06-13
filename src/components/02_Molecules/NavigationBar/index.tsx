85f57be24b1eb34fc75659fb45473e53552c5f24
MSpilari <mb.spilari@hotmail.com>
2022-06-13 16:46:39 -0300

    Envolvendo os botões do Links em uma anchor para evitar a warning React.forwardRef()

--- src/components/02_Molecules/NavigationBar/index.tsx
+++ src/components/02_Molecules/NavigationBar/index.tsx
@@ -2,7 +2,6 @@ import Link from 'next/link'
 import { AiOutlineHome } from 'react-icons/ai'
 import { BsGear, BsPerson, BsShieldLock } from 'react-icons/bs'
 import { MdWorkOutline } from 'react-icons/md'
-
 import { Button } from '../../01_Atoms/Button'
 
 const NavigationBar = () => {
@@ -11,44 +10,61 @@ const NavigationBar = () => {
 			className='flex fixed bottom-0 w-full h-12 items-center justify-around z-20 bg-zinc-900
 										lg:flex-col lg:static lg:h-96 xl:h-full'
 		>
-			<Link href='/'>
-				<Button
-					className='text-2xl text-white lg:linkButton lg:linkAnimation'
-					iconLabel={<AiOutlineHome className='lg:linkButtonSvg' />}
-					animationText={<p className='hidden lg:flex lg:linkButtonP'>Home</p>}
-				/>
+			<Link href='/' passHref>
+				<a>
+					{/* Wrap those Buttons(functional components) inside a anchor tag to avoid the warning React.forwardRef*/}
+					<Button
+						className='text-2xl text-white lg:linkButton lg:linkAnimation'
+						iconLabel={<AiOutlineHome className='lg:linkButtonSvg' />}
+						animationText={
+							<p className='hidden lg:flex lg:linkButtonP'>Home</p>
+						}
+					/>
+				</a>
 			</Link>
-			<Link href='/about'>
-				<Button
-					className='text-2xl text-white lg:linkButton lg:linkAnimation'
-					iconLabel={<BsPerson className='lg:linkButtonSvg' />}
-					animationText={<p className='hidden lg:flex lg:linkButtonP'>About</p>}
-				/>
+			<Link href='/about' passHref>
+				<a>
+					<Button
+						className='text-2xl text-white lg:linkButton lg:linkAnimation'
+						iconLabel={<BsPerson className='lg:linkButtonSvg' />}
+						animationText={
+							<p className='hidden lg:flex lg:linkButtonP'>About</p>
+						}
+					/>
+				</a>
 			</Link>
-			<Link href='/skills'>
-				<Button
-					className='text-2xl text-white lg:linkButton lg:linkAnimation'
-					iconLabel={<BsGear className='lg:linkButtonSvg' />}
-					animationText={
-						<p className='hidden lg:flex lg:linkButtonP'>Skills</p>
-					}
-				/>
+			<Link href='/skills' passHref>
+				<a>
+					<Button
+						className='text-2xl text-white lg:linkButton lg:linkAnimation'
+						iconLabel={<BsGear className='lg:linkButtonSvg' />}
+						animationText={
+							<p className='hidden lg:flex lg:linkButtonP'>Skills</p>
+						}
+					/>
+				</a>
 			</Link>
-			<Link href='/projects'>
-				<Button
-					className='text-2xl text-white lg:linkButton lg:linkAnimation'
-					iconLabel={<MdWorkOutline className='lg:linkButtonSvg' />}
-					animationText={
-						<p className='hidden lg:flex lg:linkButtonP'>Projects</p>
-					}
-				/>
+			<Link href='/projects' passHref>
+				<a>
+					<Button
+						className='text-2xl text-white lg:linkButton lg:linkAnimation'
+						iconLabel={<MdWorkOutline className='lg:linkButtonSvg' />}
+						animationText={
+							<p className='hidden lg:flex lg:linkButtonP'>Projects</p>
+						}
+					/>
+				</a>
 			</Link>
-			<Link href='/login'>
-				<Button
-					className='text-2xl text-white lg:linkButton lg:linkAnimation'
-					iconLabel={<BsShieldLock className='lg:linkButtonSvg' />}
-					animationText={<p className='hidden lg:flex lg:linkButtonP'>Login</p>}
-				/>
+			<Link href='/login' passHref>
+				<a>
+					<Button
+						className='text-2xl text-white lg:linkButton lg:linkAnimation'
+						iconLabel={<BsShieldLock className='lg:linkButtonSvg' />}
+						animationText={
+							<p className='hidden lg:flex lg:linkButtonP'>Login</p>
+						}
+					/>
+				</a>
 			</Link>
 		</nav>
 	)
