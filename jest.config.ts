10eeddd4adbeb151c536a466975732358cabc517
MSpilari <mb.spilari@hotmail.com>
2022-06-13 16:43:07 -0300

    Configuando o jest para a versão 28

--- jest.config.ts
+++ jest.config.ts
@@ -6,10 +6,11 @@ const createJestConfig = nextJest({
 
 const customJestConfig = {
 	// Add more setup options before each test is run
-	setupFilesAfterEnv: ['<rootDir>/src/tests/jest.setup.js'],
+	setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 	// if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
 	moduleDirectories: ['node_modules', '<rootDir>/'],
-	testEnvironment: 'jest-environment-jsdom'
+	testEnvironment: 'jest-environment-jsdom',
+	clearMocks: true
 }
 
 export default createJestConfig(customJestConfig)
