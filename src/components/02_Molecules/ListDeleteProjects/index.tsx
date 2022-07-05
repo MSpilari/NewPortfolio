d0a08965cea418cdbc278ac1d94a40d820a53045
MSpilari <mb.spilari@hotmail.com>
2022-07-05 12:27:22 -0300

    Adicionando um Link de retorno para Home

--- src/components/02_Molecules/ListDeleteProjects/index.tsx
+++ src/components/02_Molecules/ListDeleteProjects/index.tsx
@@ -1,4 +1,5 @@
 import { collection, onSnapshot } from 'firebase/firestore'
+import Link from 'next/link'
 import { useEffect, useState } from 'react'
 import { db } from '../../../../firebase'
 import { AdminCard } from '../../01_Atoms/AdminCard'
@@ -21,6 +22,12 @@ const ListDeleteProjects = () => {
 
 	return (
 		<div className='w-full flex flex-col items-center'>
+			<Link href={'/'}>
+				<p className='text-yellow-500 cursor-pointer font-pushster text-xl'>
+					Back to Home page
+				</p>
+			</Link>
+
 			<h1 className='text-yellow-500 text-2xl font-pushster'>
 				List/Delete Projects
 			</h1>
