27ed2b01d5ae8b5a4fcc8615e5ac0bca096eefc7
MSpilari <mb.spilari@hotmail.com>
2022-07-05 12:07:47 -0300

    Adicionando a função de deleção de projeto

--- src/components/01_Atoms/AdminCard/index.tsx
+++ src/components/01_Atoms/AdminCard/index.tsx
@@ -1,4 +1,6 @@
+import { deleteDoc, doc } from 'firebase/firestore'
 import Image from 'next/image'
+import { db } from '../../../../firebase'
 
 type IAdminCard = {
 	id: string
@@ -7,22 +9,35 @@ type IAdminCard = {
 }
 
 const AdminCard = ({ id, title, projectImage }: IAdminCard) => {
+	const deleteProject = (projectId: string) => {
+		return deleteDoc(doc(db, 'projects', projectId))
+	}
+
 	return (
 		<li
 			className='w-[60%] mx-auto my-1 h-28 text-white flex items-center justify-between 
       font-pushster border-2 border-yellow-300 rounded-lg overflow-hidden'
 			key={id}
 		>
-			<div className='w-[200px] relative h-full overflow-hidden'>
-				<Image
-					className='absolute w-full h-full'
-					objectFit='cover'
-					layout='fill'
-					src={projectImage}
-				/>
-			</div>
+			{projectImage && (
+				<div className='w-[200px] relative h-full overflow-hidden'>
+					<Image
+						className='absolute w-full h-full'
+						objectFit='cover'
+						layout='fill'
+						src={projectImage}
+					/>
+				</div>
+			)}
+
 			<h5 className='text-xl truncate'>{title}</h5>
-			<button className='mr-3 text-lg text-red-600'>X</button>
+
+			<button
+				className='mr-3 text-xl border-2 rounded-full p-2 border-red-600 text-red-600'
+				onClick={() => deleteProject(id)}
+			>
+				X
+			</button>
 		</li>
 	)
 }
