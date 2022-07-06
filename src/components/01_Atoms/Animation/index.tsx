603ef12fd3f8b735b527b39f44d1bd0001304934
MSpilari <mb.spilari@hotmail.com>
2022-07-06 12:21:25 -0300

    Removendo a função que retorna o tamanho da tela

--- src/components/01_Atoms/Animation/index.tsx
+++ src/components/01_Atoms/Animation/index.tsx
@@ -1,46 +1,15 @@
-import { HTMLAttributes, useCallback, useEffect, useState } from 'react'
+import { HTMLAttributes } from 'react'
 import Lottie from 'react-lottie-player'
 
 type AnimationProps = {
-	animationFileMobile?: object
-	animationFileDesktop?: object
+	animationFile?: object
 }
 
 const Animation: React.FC<AnimationProps & HTMLAttributes<HTMLDivElement>> = ({
-	animationFileMobile,
-	animationFileDesktop,
+	animationFile,
 	...props
 }) => {
-	const useMediaQuery = (width: string) => {
-		const [targetReached, setTargetReached] = useState(false)
-
-		const updatedTarget = useCallback((e: MediaQueryListEvent) => {
-			e.matches ? setTargetReached(true) : setTargetReached(false)
-		}, [])
-
-		useEffect(() => {
-			const media = window.matchMedia(`(min-width:${width}px)`)
-
-			media.addEventListener('change', e => updatedTarget(e))
-
-			media.matches && setTargetReached(true)
-
-			return () => media.removeEventListener('change', e => updatedTarget(e))
-		}, [])
-
-		return targetReached
-	}
-
-	return (
-		<Lottie
-			loop
-			play
-			animationData={
-				useMediaQuery('1024') ? animationFileDesktop : animationFileMobile
-			}
-			{...props}
-		/>
-	)
+	return <Lottie loop play animationData={animationFile} {...props} />
 }
 
 export { Animation }
