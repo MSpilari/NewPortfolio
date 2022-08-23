57d893ba415b6373f17817c7f8b1d9a1dccdff4b
MSpilari <mb.spilari@hotmail.com>
2022-08-23 16:58:15 -0300

    Agora o Css mostra a imagem na tela de acordo com o tamanho dela

--- src/components/01_Atoms/Logo/index.tsx
+++ src/components/01_Atoms/Logo/index.tsx
@@ -1,40 +1,26 @@
-import { useCallback, useEffect, useState } from 'react'
 import Image from 'next/image'
 
-import SmallLogo from '../../../../public/Logo.svg'
 import LargeLogo from '../../../../public/Logo(1).svg'
+import SmallLogo from '../../../../public/Logo.svg'
 
 const Logo: React.FC = () => {
-	const useMediaQuery = (width: string) => {
-		const [targetReached, setTargetReached] = useState(false)
-
-		const updatedTarget = useCallback((e: MediaQueryListEvent) => {
-			e.matches ? setTargetReached(true) : setTargetReached(false)
-		}, [])
-
-		useEffect(() => {
-			const media = window.matchMedia(`(min-width:${width}px)`)
-
-			media.addEventListener('change', e => updatedTarget(e))
-
-			media.matches && setTargetReached(true)
-
-			return () => media.removeEventListener('change', e => updatedTarget(e))
-		}, [])
-
-		return targetReached
-	}
-
 	return (
-		<div
-			className='w-40 ml-3 h-full flex items-center 
-							lg:w-full lg:ml-0 lg:justify-center'
-		>
-			<Image
-				className='object-cover'
-				src={useMediaQuery('1024') ? LargeLogo : SmallLogo}
-				alt='Logo MSpilari'
-			/>
+		<div className='w-40 ml-3 h-full flex items-center lg:w-full lg:ml-0 lg:justify-center'>
+			<div className='lg:hidden'>
+				<Image
+					className='object-cover'
+					src={SmallLogo}
+					alt='Logo MSpilari Small'
+				/>
+			</div>
+
+			<div className='hidden lg:flex'>
+				<Image
+					className='object-cover'
+					src={LargeLogo}
+					alt='Logo MSpilari Large'
+				/>
+			</div>
 		</div>
 	)
 }
