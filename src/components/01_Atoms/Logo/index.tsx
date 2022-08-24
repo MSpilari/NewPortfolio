28f2fb6abc167e9a632cf3807a68c268facded4d
MSpilari <mb.spilari@hotmail.com>
2022-08-24 11:23:43 -0300

    Colocando um altText diferente para cada logo

--- src/components/01_Atoms/Logo/index.tsx
+++ src/components/01_Atoms/Logo/index.tsx
@@ -7,11 +7,19 @@ const Logo: React.FC = () => {
 	return (
 		<div className='w-40 ml-3 h-full flex items-center lg:w-full lg:ml-0 lg:justify-center'>
 			<div className='lg:hidden'>
-				<Image className='object-cover' src={SmallLogo} alt='Logo MSpilari' />
+				<Image
+					className='object-cover'
+					src={SmallLogo}
+					alt='Logo MSpilari Mobile'
+				/>
 			</div>
 
 			<div className='hidden lg:flex'>
-				<Image className='object-cover' src={LargeLogo} alt='Logo MSpilari' />
+				<Image
+					className='object-cover'
+					src={LargeLogo}
+					alt='Logo MSpilari Desktop'
+				/>
 			</div>
 		</div>
 	)
