b2005a106e947b2269d0869090003599e9d60eca
MSpilari <mb.spilari@hotmail.com>
2022-10-31 17:26:02 -0300

    Condicionando o mostrar da animação ao tamanho da tela

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,28 +1,24 @@
-import AboutMobileFile from '../src/assets/about.json'
+import { useMediaQuery } from 'react-responsive'
 import AboutDesktopFile from '../src/assets/AboutAnimation.json'
 
 import { Animation } from '../src/components/01_Atoms/Animation'
-import { Header } from '../src/components/03_Organisms/Header'
 import { AboutText } from '../src/components/03_Organisms/AboutText'
-import { AnimationWrapper } from '../src/components/04_Templates/AnimationWrapper'
+import { Header } from '../src/components/03_Organisms/Header'
 
 const About = () => {
+	const isDesktop = useMediaQuery({ query: '(min-width:1024px)' })
+
 	return (
 		<div className='w-screen h-screen lg:flex'>
 			<Header />
 
 			<div className='flex flex-col h-full relative lg:flex-row lg:w-[calc(100%-80px)]'>
-				<AnimationWrapper className='w-full overflow-hidden absolute top-[30%] max-w-4xl sm:left-0 sm:top-[15%] lg:h-full lg:top-0 lg:left-0 lg:max-w-none'>
-					<Animation
-						animationFile={AboutMobileFile}
-						className='w-full h-[calc(100%-128px)] lg:hidden'
-					/>
-
+				{isDesktop ? (
 					<Animation
 						animationFile={AboutDesktopFile}
-						className='hidden w-full h-[calc(100%-128px)] lg:h-full lg:flex'
+						className='w-full h-[calc(100%-128px)] lg:h-full '
 					/>
-				</AnimationWrapper>
+				) : null}
 
 				<AboutText />
 			</div>
