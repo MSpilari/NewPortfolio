e544880c6400ae369c443bd8d8b946610d07adc1
MSpilari <mb.spilari@hotmail.com>
2022-11-01 12:30:49 -0300

    Removendo o css de font desnecessário

--- src/components/03_Organisms/Greet/index.tsx
+++ src/components/03_Organisms/Greet/index.tsx
@@ -4,7 +4,7 @@ const Greet: React.FC = () => {
 	const { language } = useLanguage()
 	return (
 		<div className='w-[200px] absolute bottom-0 left-[25%] z-10 text-white flex flex-col sm:w-[400px] lg:left-[15%] lg:bottom-[10%]'>
-			<p className='font-pushster text-lg my-2 lg:text-4xl'>
+			<p className='text-lg my-2 lg:text-4xl'>
 				{language.Hello1}&#128075;{language.Hello2}
 			</p>
 
@@ -12,9 +12,7 @@ const Greet: React.FC = () => {
 				{language.Name}
 			</h1>
 
-			<p className='font-pushster ml-auto text-red-600 my-2 lg:text-2xl'>
-				{language.Role}
-			</p>
+			<p className='ml-auto text-red-600 my-2 lg:text-2xl'>{language.Role}</p>
 		</div>
 	)
 }
