5a2366e3f1c22390d312301ec073af8cad680423
MSpilari <mb.spilari@hotmail.com>
2022-11-01 12:30:14 -0300

    Adicionando a props sizes na Image

--- src/components/01_Atoms/Logo/index.tsx
+++ src/components/01_Atoms/Logo/index.tsx
@@ -8,11 +8,13 @@ const Logo: React.FC = () => {
 	const isMobile = useMediaQuery({ query: '(max-width:768px)' })
 
 	return (
-		<div className='w-40 ml-3 h-full flex items-center lg:w-full lg:ml-0 lg:justify-center'>
+		<div className='w-40 ml-3 h-full flex items-center relative lg:w-full lg:ml-0 lg:justify-center'>
 			<Image
-				className='object-cover'
 				src={isMobile ? LogoMobile : LogoDesktop}
 				alt='Logo MSpilari Mobile'
+				objectFit='contain'
+				layout='fill'
+				sizes='25vw'
 			/>
 		</div>
 	)
