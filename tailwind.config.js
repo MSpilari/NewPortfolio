0e4f1fad37c00d9cdcf1ad254f586444b86a2afe
MSpilari <mb.spilari@hotmail.com>
2022-11-09 17:09:48 -0300

    Criando as animações do loading component

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,54 @@ module.exports = {
 			fontFamily: {
 				theNautigal: ['The Nautigal', 'cursive'],
 				pushster: ['Pushster', 'cursive']
+			},
+			keyframes: {
+				XFtransition: {
+					'0%': {
+						left: '5rem',
+						width: '0px'
+					},
+					'100%': {
+						left: '5rem',
+						width: 'calc(100vw - 5rem)'
+					}
+				},
+				XBtransition: {
+					'0%': {
+						left: '5rem',
+						width: 'calc(100vw - 5rem)'
+					},
+					'100%': {
+						left: '5rem',
+						width: '0px'
+					}
+				},
+				YFtransition: {
+					'0%': {
+						top: '5rem',
+						height: '0px'
+					},
+					'100%': {
+						top: '5rem',
+						height: 'calc(100vh - 8rem)'
+					}
+				},
+				YBtransition: {
+					'0%': {
+						top: '5rem',
+						height: 'calc(100vw - 8rem)'
+					},
+					'100%': {
+						top: '5rem',
+						height: '0px'
+					}
+				}
+			},
+			animation: {
+				XFtransition: 'XFtransition 0.3s ease-out forwards',
+				XBtransition: 'XBtransition 1s ease-in-out',
+				YFtransition: 'YFtransition 0.3s ease-out forwards',
+				YBtransition: 'YBtransition 1s ease-in'
 			}
 		}
 	},
