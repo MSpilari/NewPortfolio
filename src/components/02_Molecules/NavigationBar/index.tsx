4892c0bb5cc13e4b24baa085fa9beb646e259a24
MSpilari <mb.spilari@hotmail.com>
2024-01-10 16:31:11 -0300

    Removendo as anchors de dentro dos Links, seguindo o padrão do next 14

--- src/components/02_Molecules/NavigationBar/index.tsx
+++ src/components/02_Molecules/NavigationBar/index.tsx
@@ -8,74 +8,57 @@ const NavigationBar = () => {
 	return (
 		<nav className='flex fixed bottom-0 w-full h-12 items-center justify-around z-20 bg-zinc-900 lg:flex-col lg:static lg:h-96 xl:h-full'>
 			<Link href='/' passHref>
-				<a data-testid='NavLink'>
-					{/* Wrap those Buttons(functional components) inside a anchor tag to avoid the warning React.forwardRef*/}
-					<Button
-						data-testid='homeButton'
-						className='text-2xl text-white lg:linkButton lg:linkAnimation'
-						iconLabel={<AiOutlineHome className='lg:linkButtonSvg' />}
-						animationText={
-							<p className='hidden lg:flex lg:linkButtonP'>Home</p>
-						}
-						aria-label='Home Button'
-					/>
-				</a>
+				<Button
+					data-testid='homeButton'
+					className='text-2xl text-white lg:linkButton lg:linkAnimation'
+					iconLabel={<AiOutlineHome className='lg:linkButtonSvg' />}
+					animationText={<p className='hidden lg:flex lg:linkButtonP'>Home</p>}
+					aria-label='Home Button'
+				/>
 			</Link>
 
 			<Link href='/about' passHref>
-				<a data-testid='NavLink'>
-					<Button
-						data-testid='aboutButton'
-						className='text-2xl text-white lg:linkButton lg:linkAnimation'
-						iconLabel={<BsPerson className='lg:linkButtonSvg' />}
-						animationText={
-							<p className='hidden lg:flex lg:linkButtonP'>About</p>
-						}
-						aria-label='About Button'
-					/>
-				</a>
+				<Button
+					data-testid='aboutButton'
+					className='text-2xl text-white lg:linkButton lg:linkAnimation'
+					iconLabel={<BsPerson className='lg:linkButtonSvg' />}
+					animationText={<p className='hidden lg:flex lg:linkButtonP'>About</p>}
+					aria-label='About Button'
+				/>
 			</Link>
 
 			<Link href='/skills' passHref>
-				<a data-testid='NavLink'>
-					<Button
-						data-testid='skillsButton'
-						className='text-2xl text-white lg:linkButton lg:linkAnimation'
-						iconLabel={<BsGear className='lg:linkButtonSvg' />}
-						animationText={
-							<p className='hidden lg:flex lg:linkButtonP'>Skills</p>
-						}
-						aria-label='Skills Button'
-					/>
-				</a>
+				<Button
+					data-testid='skillsButton'
+					className='text-2xl text-white lg:linkButton lg:linkAnimation'
+					iconLabel={<BsGear className='lg:linkButtonSvg' />}
+					animationText={
+						<p className='hidden lg:flex lg:linkButtonP'>Skills</p>
+					}
+					aria-label='Skills Button'
+				/>
 			</Link>
 
 			<Link href='/projects' passHref>
-				<a data-testid='NavLink'>
-					<Button
-						data-testid='projectsButton'
-						className='text-2xl text-white lg:linkButton lg:linkAnimation'
-						iconLabel={<MdWorkOutline className='lg:linkButtonSvg' />}
-						animationText={
-							<p className='hidden lg:flex lg:linkButtonP'>Projects</p>
-						}
-						aria-label='Projects Button'
-					/>
-				</a>
+				<Button
+					data-testid='projectsButton'
+					className='text-2xl text-white lg:linkButton lg:linkAnimation'
+					iconLabel={<MdWorkOutline className='lg:linkButtonSvg' />}
+					animationText={
+						<p className='hidden lg:flex lg:linkButtonP'>Projects</p>
+					}
+					aria-label='Projects Button'
+				/>
 			</Link>
 
 			<Link href='/login' passHref>
-				<a data-testid='NavLink'>
-					<Button
-						data-testid='loginButton'
-						className='text-2xl text-white lg:linkButton lg:linkAnimation'
-						iconLabel={<BsShieldLock className='lg:linkButtonSvg' />}
-						animationText={
-							<p className='hidden lg:flex lg:linkButtonP'>Login</p>
-						}
-						aria-label='Login Button'
-					/>
-				</a>
+				<Button
+					data-testid='loginButton'
+					className='text-2xl text-white lg:linkButton lg:linkAnimation'
+					iconLabel={<BsShieldLock className='lg:linkButtonSvg' />}
+					animationText={<p className='hidden lg:flex lg:linkButtonP'>Login</p>}
+					aria-label='Login Button'
+				/>
 			</Link>
 		</nav>
 	)
