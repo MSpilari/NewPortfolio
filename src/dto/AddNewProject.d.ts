5d5cd183432bde3d3dc85d52d1c00b5c34cc2032
MSpilari <mb.spilari@hotmail.com>
2024-01-13 17:21:07 -0300

    Criação da interface da adição dos novos projetos

---
+++ src/dto/AddNewProject.d.ts
@@ -0,0 +1,18 @@
+export interface IAllInfo {
+	image: string
+	title: string
+	githubLink: string
+	projectLink: string
+}
+
+export interface IAllProjects {
+	id: string
+	title: string
+	projectImage: string
+}
+
+export interface IAdminCard {
+	id: string
+	title: string
+	projectImage: string
+}
