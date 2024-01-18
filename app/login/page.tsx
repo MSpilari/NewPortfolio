56c5f1ca9bb61fb895d87863da0a75e8dd08cfff
MSpilari <mb.spilari@hotmail.com>
2024-01-18 17:56:17 -0300

    Removendo a animação e inserindo o react-hook-forms

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,18 +1,8 @@
-import LoginFileAnimationDesktop from '@assets/animationLoginDesktop.json'
-import { Animation } from '@components/01_Atoms/Animation'
-import { AnimationWrapper } from '@components/04_Templates/AnimationWrapper'
 import { LoginForm } from '@components/03_Organisms/LoginForm'
 
 const Login = () => {
 	return (
 		<div className='flex flex-col h-[calc(100%-128px)] relative lg:flex-row lg:w-[calc(100%-80px)] lg:h-full '>
-			<AnimationWrapper>
-				<Animation
-					className='hidden w-full h-full lg:flex'
-					animationFile={LoginFileAnimationDesktop}
-				/>
-			</AnimationWrapper>
-
 			<LoginForm />
 		</div>
 	)
