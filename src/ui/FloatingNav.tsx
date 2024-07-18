335e05d046a87ba670d2ab2a2ea99e27bf7a3970
MSpilari <mb.spilari@hotmail.com>
2024-07-18 12:24:44 -0300

    refactor: remove comments

--- src/ui/FloatingNav.tsx
+++ src/ui/FloatingNav.tsx
@@ -16,7 +16,6 @@ export const FloatingNav = ({ navItems, className }: FloatingNavDTO) => {
   const [visible, setVisible] = useState(false);
 
   useMotionValueEvent(scrollYProgress, "change", (current) => {
-    // Check if current is not undefined and is a number
     if (typeof current === "number") {
       let direction = current! - scrollYProgress.getPrevious()!;
 
@@ -63,10 +62,6 @@ export const FloatingNav = ({ navItems, className }: FloatingNavDTO) => {
             <span className="hidden sm:block text-sm">{navItem.name}</span>
           </Link>
         ))}
-        {/* <button className="border text-sm font-medium relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white px-4 py-2 rounded-full">
-          <span>Login</span>
-          <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent  h-px" />
-        </button> */}
       </motion.div>
     </AnimatePresence>
   );
