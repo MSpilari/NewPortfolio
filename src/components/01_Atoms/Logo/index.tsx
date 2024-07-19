a182fb0ce825dd60bd929daa48d905129f06b326
MSpilari <mb.spilari@hotmail.com>
2024-07-19 17:12:04 -0300

    refactor: remove the width to prevent ui bug

--- src/components/01_Atoms/Logo/index.tsx
+++ src/components/01_Atoms/Logo/index.tsx
@@ -1,22 +1,22 @@
-import Image from 'next/image'
-import { useMediaQuery } from 'react-responsive'
+import Image from "next/image";
+import { useMediaQuery } from "react-responsive";
 
-import LogoMobile from '@public/LogoMobile.png'
-import LogoDesktop from '@public/Logo.png'
+import LogoMobile from "@public/LogoMobile.png";
+import LogoDesktop from "@public/Logo.png";
 
 const Logo: React.FC = () => {
-	const isMobile = useMediaQuery({ query: '(max-width:768px)' })
+  const isMobile = useMediaQuery({ query: "(max-width:768px)" });
 
-	return (
-		<div className='w-40 ml-3 h-full flex items-center relative lg:w-full lg:ml-0 lg:justify-center'>
-			<Image
-				className='w-full h-full object-contain'
-				src={isMobile ? LogoMobile : LogoDesktop}
-				alt='Logo MSpilari'
-				sizes='25vw'
-			/>
-		</div>
-	)
-}
+  return (
+    <div className="w-40 ml-3 h-full flex items-center relative lg:w-full lg:ml-0 lg:justify-center">
+      <Image
+        className="w-full h-full object-contain"
+        src={isMobile ? LogoMobile : LogoDesktop}
+        alt="Logo MSpilari"
+        sizes="25vw"
+      />
+    </div>
+  );
+};
 
-export { Logo }
+export { Logo };
