5ba63b5d153dbb155fe2fc7e263cdd2ee5343e5b
MSpilari <mb.spilari@hotmail.com>
2024-07-19 17:10:35 -0300

    refactor: Add overflow-hidden to prevent scroll

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,39 +1,39 @@
-import '../src/styles/globals.css'
-import { Metadata } from 'next'
-import { lobster, theNautigal } from '@config/fonts'
-import { AuthenticationProvider } from '@context/authentication'
-import { LanguageProvider } from '@context/language'
-import { Header } from '@components/03_Organisms/Header'
+import "../src/styles/globals.css";
+import { Metadata } from "next";
+import { lobster, theNautigal } from "@config/fonts";
+import { AuthenticationProvider } from "@context/authentication";
+import { LanguageProvider } from "@context/language";
+import { Header } from "@components/03_Organisms/Header";
 
 export const metadata: Metadata = {
-	title: 'MSpilari | Fullstack Developer',
-	description:
-		'Matheus Bernardes Spilari, MSpilari, FullStack Developer, portfólio.'
-}
+  title: "MSpilari | Fullstack Developer",
+  description:
+    "Matheus Bernardes Spilari, MSpilari, FullStack Developer, portfólio.",
+};
 
 export default function RootLayout({
-	children
+  children,
 }: {
-	children: React.ReactNode
+  children: React.ReactNode;
 }) {
-	return (
-		<html
-			lang='en'
-			className={`${lobster.variable} ${theNautigal.variable} font-lobster`}
-		>
-			<link rel='icon' href='/Logo.png' />
+  return (
+    <html
+      lang="en"
+      className={`${lobster.variable} ${theNautigal.variable} font-lobster`}
+    >
+      <link rel="icon" href="/Logo.png" />
 
-			<AuthenticationProvider>
-				<LanguageProvider>
-					<body>
-						<div className='w-screen h-screen lg:flex'>
-							<Header />
+      <AuthenticationProvider>
+        <LanguageProvider>
+          <body>
+            <main className="w-dvw h-dvh lg:flex overflow-hidden">
+              <Header />
 
-							{children}
-						</div>
-					</body>
-				</LanguageProvider>
-			</AuthenticationProvider>
-		</html>
-	)
+              {children}
+            </main>
+          </body>
+        </LanguageProvider>
+      </AuthenticationProvider>
+    </html>
+  );
 }
