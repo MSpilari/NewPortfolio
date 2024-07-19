b28bcf939b5e7e325c31274b00f487d20643ff4f
MSpilari <mb.spilari@hotmail.com>
2024-07-19 16:00:06 -0300

    refactor: remove comments

--- src/components/Clients/Clients.tsx
+++ src/components/Clients/Clients.tsx
@@ -14,10 +14,7 @@ const Clients = () => {
       </h1>
 
       <div className="flex flex-col items-center max-lg:mt-10">
-        <div
-          // remove bg-white dark:bg-black dark:bg-grid-white/[0.05], h-[40rem] to 30rem , md:h-[30rem] are for the responsive design
-          className="h-[50vh] md:h-[30rem] rounded-md flex flex-col antialiased  items-center justify-center relative overflow-hidden"
-        >
+        <div className="h-[50vh] md:h-[30rem] rounded-md flex flex-col antialiased  items-center justify-center relative overflow-hidden">
           <InfiniteMovingCards
             items={testimonials}
             direction="right"
