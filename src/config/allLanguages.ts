6ece0a3231373c0887db15a28c46961bde2f90a7
MSpilari <mb.spilari@hotmail.com>
2024-07-20 17:21:26 -0300

    feat: add texts to the new experience page

--- src/config/allLanguages.ts
+++ src/config/allLanguages.ts
@@ -1,14 +1,37 @@
 const allLanguages = {
-	ptBr: {
-		Hello1: 'Olá, ',
-		Hello2: ' eu sou o',
-		Name: ' Matheus B. Spilari',
-		Role: 'Fullstack Developer',
-		HomeTitle: 'Olá, eu sou o Matheus, Web Developer.',
-		ButtonLabel: 'Entrar em contato !',
-		ButtonMessageLabel: 'Enviar Mensagem !',
-		AboutTitle: 'Sobre Mim',
-		AboutInfo: `Olá, eu sou Matheus B. Spilari, um Engenheiro de Software com expertise em uma variedade de linguagens e tecnologias.
+  ptBr: {
+    Hello1: "Olá, ",
+    Hello2: " eu sou o",
+    Name: " Matheus B. Spilari",
+    Role: "Fullstack Developer",
+    HomeTitle: "Olá, eu sou o Matheus, Web Developer.",
+    ButtonLabel: "Entrar em contato !",
+    ButtonMessageLabel: "Enviar Mensagem !",
+    AboutTitle: "Experiências",
+    Experience: [
+      {
+        expImg: "/exp1.svg",
+        expDate: "2012 -> 2014",
+        expName: "Desenvolvedor Frontend",
+      },
+      {
+        expImg: "/exp2.svg",
+        expDate: "2014 -> 2016",
+        expName: "Desenvolvedor Backend",
+      },
+      {
+        expImg: "/exp3.svg",
+        expDate: "2014 ->  presente",
+        expName: "Desenvolvedor Freelancer Web ",
+      },
+      {
+        expImg: "/exp4.svg",
+        expDate: "2016 -> presente",
+        expName: "Desenvolvedor Fullstack ",
+      },
+    ],
+    ExperienceButton: "Mais no",
+    AboutInfo: `Olá, eu sou Matheus B. Spilari, um Engenheiro de Software com expertise em uma variedade de linguagens e tecnologias.
 		
 		Minha jornada profissional foi marcada por desafios empolgantes e projetos inovadores, impulsionados pelo meu comprometimento com a excelência técnica e a busca incessante pela melhoria contínua. Com uma sólida base em linguagens como TypeScript, Java e Python, e fluência em frameworks como NextJs, NestJs, e tecnologias como Docker, Node, SpringBoot, React, Angular, MongoDb e PostgreSQL, tenho liderado o desenvolvimento de soluções robustas e escaláveis.
 		
@@ -20,54 +43,77 @@ const allLanguages = {
 		
 		Explore meu portfólio para uma visão mais detalhada dos projetos nos quais estive envolvido e como minha abordagem pode agregar valor ao seu próximo desafio.`,
 
-		SkillsTitle: 'Habilidades & Experiência',
-		SkillsInfo: [
-			{
-				title: 'Linguagens',
-				text: `Como Engenheiro de Software, trago uma sólida experiência tanto no desenvolvimento Frontend quanto Backend, demonstrando habilidades especializadas e uma abordagem orientada a resultados. Contribuição para o desenvolvimento de projetos de pequena, média e grande escala, aplicando metodologias ágeis, como Kanban, para maximizar a eficiência.`,
-				category: 'languages'
-			},
-			{
-				title: 'Front End',
-				text: `4+ anos de experiência em React, NextJs e Angular 
+    SkillsTitle: "Habilidades",
+    SkillsInfo: [
+      {
+        title: "Linguagens",
+        text: `Como Engenheiro de Software, trago uma sólida experiência tanto no desenvolvimento Frontend quanto Backend, demonstrando habilidades especializadas e uma abordagem orientada a resultados. Contribuição para o desenvolvimento de projetos de pequena, média e grande escala, aplicando metodologias ágeis, como Kanban, para maximizar a eficiência.`,
+        category: "languages",
+      },
+      {
+        title: "Front End",
+        text: `4+ anos de experiência em React, NextJs e Angular 
 				
 				Proficiência em construir interfaces web responsivas, priorizando o conceito Mobile First e seguindo as melhores práticas de Single Page Applications (SPAs).
 							
 				Experiência em desenvolver aplicações web dinâmicas e interativas, incorporando animações e layouts inovadores.`,
-				category: 'frontend'
-			},
-			{
-				title: 'Back End',
-				text: `3+ anos de experiência em NodeJs e Java para a construção de APIs no padrão RESTful/REST, utilizando Express, NestJs, SpringBoot.
+        category: "frontend",
+      },
+      {
+        title: "Back End",
+        text: `3+ anos de experiência em NodeJs e Java para a construção de APIs no padrão RESTful/REST, utilizando Express, NestJs, SpringBoot.
 				
 				Implementação de práticas avançadas de autenticação, incluindo JWT e OAuth2.0.
 				
 				Profundo conhecimento em bancos de dados como PostgreSQL, MongoDB e Firebase, garantindo eficiência e segurança nas operações.`,
-				category: 'backend'
-			},
-			{
-				title: 'Testes',
-				text: `Experiência sólida em implementação de testes unitários, integração e E2E utilizando Jest e Cypress.
+        category: "backend",
+      },
+      {
+        title: "Testes",
+        text: `Experiência sólida em implementação de testes unitários, integração e E2E utilizando Jest e Cypress.
 
 				Proficiência na criação de testes automatizados robustos para garantir a qualidade e a estabilidade do código em todas as fases do desenvolvimento.
 				
 				Implementação de estratégias abrangentes de teste, abordando tanto o frontend quanto o backend, para assegurar a confiabilidade e o desempenho de sistemas complexos.`,
-				category: 'tests'
-			}
-		],
-		ProjectsTitle: 'Projetos',
-		ContactTitle: 'Me Contate !'
-	},
-	en: {
-		Hello1: 'Hello, ',
-		Hello2: ' i´m',
-		Name: ' Matheus B. Spilari',
-		Role: 'Fullstack Developer',
-		HomeTitle: 'Hello, I´m Matheus, Web Developer.',
-		ButtonLabel: 'Contact Me !',
-		ButtonMessageLabel: 'Send a Message !',
-		AboutTitle: 'About Me',
-		AboutInfo: `Hello, I'm Matheus B. Spilari, a Software Engineer with expertise in a variety of languages and technologies.
+        category: "tests",
+      },
+    ],
+    ProjectsTitle: "Projetos",
+    ContactTitle: "Me Contate !",
+  },
+  en: {
+    Hello1: "Hello, ",
+    Hello2: " i´m",
+    Name: " Matheus B. Spilari",
+    Role: "Fullstack Developer",
+    HomeTitle: "Hello, I´m Matheus, Web Developer.",
+    ButtonLabel: "Contact Me !",
+    ButtonMessageLabel: "Send a Message !",
+    Experience: [
+      {
+        expImg: "/exp1.svg",
+        expDate: "2013 -> 2015",
+        expName: "Frontend Developer",
+      },
+      {
+        expImg: "/exp2.svg",
+        expDate: "2015 -> 2016",
+        expName: "Backend Developer",
+      },
+      {
+        expImg: "/exp3.svg",
+        expDate: "2014 ->  present",
+        expName: "Freelancer Web Developer",
+      },
+      {
+        expImg: "/exp4.svg",
+        expDate: "2016 -> present",
+        expName: "Fullstack Developer",
+      },
+    ],
+    ExperienceButton: "More on ",
+    AboutTitle: "Experience",
+    AboutInfo: `Hello, I'm Matheus B. Spilari, a Software Engineer with expertise in a variety of languages and technologies.
 
 		My professional journey has been marked by exciting challenges and innovative projects, driven by my commitment to technical excellence and a relentless pursuit of continuous improvement. With a solid foundation in languages such as TypeScript, Java, and Python, and proficiency in frameworks like Next.js, Nest.js, along with technologies including Docker, Node.js, Spring Boot, React, Angular, MongoDB, and PostgreSQL, I have led the development of robust and scalable solutions.
 		
@@ -79,44 +125,44 @@ const allLanguages = {
 		
 		Explore my portfolio for a more detailed view of the projects I've been involved in and how my approach can add value to your next challenge.
 		`,
-		SkillsTitle: 'Skills & Experience',
-		SkillsInfo: [
-			{
-				title: 'Languages',
-				text: `As a Software Engineer, I bring a robust experience in both Frontend and Backend development, showcasing specialized skills and a results-oriented approach. I have contributed to the development of projects of small, medium, and large scales, applying agile methodologies such as Kanban to maximize efficiency.`,
-				category: 'languages'
-			},
-			{
-				title: 'Front End',
-				text: `4+ years of experience in React, Next.js, and Angular.
+    SkillsTitle: "Skills",
+    SkillsInfo: [
+      {
+        title: "Languages",
+        text: `As a Software Engineer, I bring a robust experience in both Frontend and Backend development, showcasing specialized skills and a results-oriented approach. I have contributed to the development of projects of small, medium, and large scales, applying agile methodologies such as Kanban to maximize efficiency.`,
+        category: "languages",
+      },
+      {
+        title: "Front End",
+        text: `4+ years of experience in React, Next.js, and Angular.
 
 				Proficiency in building responsive web interfaces, prioritizing the Mobile First concept, and following best practices for Single Page Applications (SPAs).
 				
 				Experience in developing dynamic and interactive web applications, incorporating animations and innovative layouts.`,
-				category: 'frontend'
-			},
-			{
-				title: 'Back End',
-				text: `3+ years of experience in Node.js and Java for building APIs following the RESTful/REST pattern, using Express, Nest.js, and Spring Boot.
+        category: "frontend",
+      },
+      {
+        title: "Back End",
+        text: `3+ years of experience in Node.js and Java for building APIs following the RESTful/REST pattern, using Express, Nest.js, and Spring Boot.
 
 				Implementation of advanced authentication practices, including JWT and OAuth 2.0.
 				
 				In-depth knowledge of databases such as PostgreSQL, MongoDB, and Firebase, ensuring efficiency and security in operations.`,
-				category: 'backend'
-			},
-			{
-				title: 'Tests',
-				text: `Solid experience in implementing unit, integration, and E2E tests using Jest and Cypress.
+        category: "backend",
+      },
+      {
+        title: "Tests",
+        text: `Solid experience in implementing unit, integration, and E2E tests using Jest and Cypress.
 
 				Proficiency in creating robust automated tests to ensure code quality and stability throughout all stages of development.
 				
 				Implementation of comprehensive testing strategies, addressing both frontend and backend, to ensure reliability and performance of complex systems.`,
-				category: 'tests'
-			}
-		],
-		ProjectsTitle: 'Projects',
-		ContactTitle: 'Contact Me !'
-	}
-}
+        category: "tests",
+      },
+    ],
+    ProjectsTitle: "Projects",
+    ContactTitle: "Contact Me !",
+  },
+};
 
-export { allLanguages }
+export { allLanguages };
