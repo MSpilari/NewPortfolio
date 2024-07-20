6688a1fa31f526f29e7d4d14fef507b9ae70fd92
MSpilari <mb.spilari@hotmail.com>
2024-07-20 17:23:46 -0300

    refactor: add types of the experince page objects

--- src/dto/LanguageContext.d.ts
+++ src/dto/LanguageContext.d.ts
@@ -1,30 +1,32 @@
-import { Dispatch, SetStateAction, ReactNode } from 'react'
+import { Dispatch, SetStateAction, ReactNode } from "react";
 
 interface ILanguageProvider {
-	children: ReactNode
+  children: ReactNode;
 }
 
 interface ILanguage {
-	Hello1: string
-	Hello2: string
-	Name: string
-	Role: string
-	HomeTitle: string
-	ButtonLabel: string
-	ButtonMessageLabel: string
-	AboutTitle: string
-	AboutInfo: string
-	SkillsTitle: string
-	SkillsInfo: { title: string; text: string; category: string }[]
-	ProjectsTitle: string
-	ContactTitle: string
+  Hello1: string;
+  Hello2: string;
+  Name: string;
+  Role: string;
+  HomeTitle: string;
+  ButtonLabel: string;
+  ButtonMessageLabel: string;
+  AboutTitle: string;
+  AboutInfo: string;
+  Experience: { expImg: string; expDate: string; expName: string }[];
+  ExperienceButton: string;
+  SkillsTitle: string;
+  SkillsInfo: { title: string; text: string; category: string }[];
+  ProjectsTitle: string;
+  ContactTitle: string;
 }
 
-type allLogosCategory = 'frontend' | 'backend' | 'tests' | 'languages'
+type allLogosCategory = "frontend" | "backend" | "tests" | "languages";
 
 interface IDefaultLanguage {
-	language: ILanguage
-	setLanguage: Dispatch<SetStateAction<ILanguage>>
+  language: ILanguage;
+  setLanguage: Dispatch<SetStateAction<ILanguage>>;
 }
 
-export { IDefaultLanguage, ILanguageProvider, allLogosCategory }
+export { IDefaultLanguage, ILanguageProvider, allLogosCategory };
