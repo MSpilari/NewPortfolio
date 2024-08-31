d21c7476505e8f7438993bb2ebc436425665b846
MSpilari <mb.spilari@hotmail.com>
2024-08-31 16:24:19 -0300

    feat: add Stack component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import { Header } from "@/src/components/Header";
 import { Hero } from "@/src/components/Hero";
+import { Stack } from "@/src/components/Stack";
 
 export default function Home() {
   return (
     <main className="bg-zinc-950 w-full text-white overflow-x-hidden">
       <Header />
       <Hero />
+      <Stack />
     </main>
   );
 }
