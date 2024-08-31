f49b5f2fb58019b691e5356cb926969858e4ded0
MSpilari <mb.spilari@hotmail.com>
2024-08-31 16:22:11 -0300

    feat(words): create to use at Hero component

---
+++ src/data/words.ts
@@ -0,0 +1,9 @@
+const words = [
+  { text: "Hello." },
+  { text: "I'm" },
+  { text: "Matheus", className: "text-yellow-500" },
+  { text: "B.", className: "text-yellow-500" },
+  { text: "Spilari.", className: "text-yellow-500" },
+];
+
+export { words };
