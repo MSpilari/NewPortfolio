247b7066f283d88c2aeb3de34884875cc428990c
MSpilari <mb.spilari@hotmail.com>
2024-08-31 13:39:22 -0300

    First commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,5 @@
-"use client";
-
-import { useMediaQuery } from "react-responsive";
-
-import AnimationDesktopFile from "@assets/indexAnimationDesktop.json";
-import AnimationMobileFile from "@assets/indexAnimationMobile.json";
-
-import { Animation } from "@components/01_Atoms/Animation";
-import { Greet } from "@components/03_Organisms/Greet";
-
-const HomePage = () => {
-  const isMobile = useMediaQuery({ query: "(max-width:768px)" });
-
+export default function Home() {
   return (
-    <div className="flex flex-col h-[calc(100%-128px)] relative lg:flex-row lg:w-[calc(100%-80px)] lg:h-full ">
-      <Animation
-        animationFile={isMobile ? AnimationMobileFile : AnimationDesktopFile}
-        className="w-full h-full"
-      />
-      <Greet />
-    </div>
+    <main className="bg-zinc-950 w-full text-white overflow-x-hidden"></main>
   );
-};
-
-export default HomePage;
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,32 @@
+import type { Config } from "tailwindcss";
+
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [addVariablesForColors],
+};
+
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
+
+export default config;
