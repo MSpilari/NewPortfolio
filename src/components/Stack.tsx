ae2ef68b0186088b454e728d79b7c75f58784ac4
MSpilari <mb.spilari@hotmail.com>
2024-08-31 16:23:32 -0300

    feat: create Stack component

---
+++ src/components/Stack.tsx
@@ -0,0 +1,17 @@
+import { stackIcons } from "../data/stackIcons";
+import { StackIcon } from "./StackIcon";
+
+const Stack = () => {
+  return (
+    <section className="w-screen flex flex-col items-center">
+      <h1 className="text-yellow-500 text-3xl">Stack</h1>
+      <div className="flex flex-wrap w-[95%] mx-auto items-center justify-center">
+        {stackIcons.map((icon) => (
+          <StackIcon icon={icon} key={icon.url} />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export { Stack };
