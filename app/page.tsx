ee245da2279757287808dfea8bbca25f092fe5e0
MSpilari <mb.spilari@hotmail.com>
2024-08-31 16:20:02 -0300

    feat: add Hero component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { Header } from "@/src/components/Header";
+import { Hero } from "@/src/components/Hero";
 
 export default function Home() {
   return (
     <main className="bg-zinc-950 w-full text-white overflow-x-hidden">
       <Header />
+      <Hero />
     </main>
   );
 }
