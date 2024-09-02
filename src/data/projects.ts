31559d3dc980afbcf82ba519ef43c474d3600a7b
MSpilari <mb.spilari@hotmail.com>
2024-09-02 16:42:16 -0300

    feat: create a dummy data for projects

---
+++ src/data/projects.ts
@@ -0,0 +1,12 @@
+const projects = [
+  {
+    title: " Make things float in air",
+    description: "Hover over this card to unleash the power of CSS perspective",
+    imageUrl: "/projectImage.jpg",
+    githubRepo: "",
+    url: "",
+    techs: ["/reactjs.png", "/tailwindcss.png", "/spring.png", "/mongodb.png"],
+  },
+];
+
+export { projects };
