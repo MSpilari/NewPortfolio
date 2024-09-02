129fdaf3f43f17779c82cb817c6b9ff079c2c102
MSpilari <mb.spilari@hotmail.com>
2024-09-02 18:06:11 -0300

    feat: create Blog component

---
+++ src/components/Blog.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const Blog = () => {
+  return (
+    <section className="w-full flex flex-col items-center justify-center">
+      <h1 className="text-yellow-500 text-3xl">Blog</h1>
+    </section>
+  );
+};
+
+export { Blog };
