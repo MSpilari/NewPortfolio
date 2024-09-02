cbdcbe148ea86c77ce23eadb8da4955288cedf31
MSpilari <mb.spilari@hotmail.com>
2024-09-02 12:17:30 -0300

    feat: create experience data to be consumed by component

---
+++ src/data/experience.ts
@@ -0,0 +1,40 @@
+const data = [
+  {
+    title: "2021",
+    content: {
+      role: "Fullstack Developer",
+      employer: "Rhegional Consultoria de Imóveis",
+      activities: [
+        "Desenvolvimento de aplicações utilizando Java/Spring e NodeJS.",
+        "Criação de páginas com frameworks Javascript(NextJs e AngularJs).",
+        "Implementação e integração com APIs REST.",
+      ],
+    },
+  },
+  {
+    title: "2019",
+    content: {
+      role: "Backend Developer",
+      employer: "ByteCraft Tech Innovations",
+      activities: [
+        "Desenvolvimento com Node.js e Java/Spring Boot.",
+        "Bancos de Dados Relacionais e Não-Relacionais.",
+        "Segurança e Autenticação com JWT.",
+      ],
+    },
+  },
+  {
+    title: "2017",
+    content: {
+      role: "Frontend Developer",
+      employer: "WaveLine Creative",
+      activities: [
+        "Desenvolvimento com React e Angular.",
+        "Design e Prototipação com Figma.",
+        "Foco na otimização de performance e acessibilidade das aplicações.",
+      ],
+    },
+  },
+];
+
+export { data };
