e9249b388f6967d143668bf3c7b329b98e85c8f6
MSpilari <mb.spilari@hotmail.com>
2024-09-02 12:19:21 -0300

    feat(Logo image): add priority property

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,12 @@ import { MdWork } from "react-icons/md";
 const Header = () => {
   return (
     <header className="w-full h-16 flex items-center justify-between px-4">
-      <Image src={Logo} alt="Logo" className="h-full w-36 sm:w-40 md:w-48 " />
+      <Image
+        src={Logo}
+        alt="Logo"
+        className="h-full w-36 sm:w-40 md:w-48"
+        priority={true}
+      />
 
       <nav className="flex gap-4 items-center text-xl lg:text-2xl z-50">
         <Link href="#stack" className="hidden md:block">
