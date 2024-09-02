7fc7ff68d1eee7c2b69e838c404cbb133d0ba76d
MSpilari <mb.spilari@hotmail.com>
2024-09-02 16:47:48 -0300

    feat: create Projects component

---
+++ src/components/Projects.tsx
@@ -0,0 +1,25 @@
+import { projects } from "@/src/data/projects";
+import { ProjectCard } from "./ProjectCard";
+
+const Projects = () => {
+  return (
+    <section className="w-full flex flex-col items-center justify-center">
+      <h1 className="text-yellow-500 text-3xl">Projects</h1>
+      <div className="w-[90%] flex items-center justify-center flex-wrap gap-x-3 mx-auto">
+        {projects.map((project) => (
+          <ProjectCard
+            title={project.title}
+            description={project.description}
+            githubRepo={project.githubRepo}
+            imageUrl={project.imageUrl}
+            url={project.url}
+            techs={project.techs}
+            key={project.url}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export { Projects };
