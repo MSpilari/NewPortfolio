8be7fe994e3035073c3838b1ae261f83757b55ee
MSpilari <mb.spilari@hotmail.com>
2024-09-03 13:15:46 -0300

    build: add the host where the images come from

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "media.dev.to",
+        pathname: "/**",
+        port: "",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
