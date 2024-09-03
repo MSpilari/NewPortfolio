5766a2758a25f38e8c53495c2cf44aebf179a4b8
MSpilari <mb.spilari@hotmail.com>
2024-09-03 17:12:16 -0300

    refactor: add id="stack" prop

--- src/components/Stack.tsx
+++ src/components/Stack.tsx
@@ -3,7 +3,7 @@ import { StackIcon } from "./StackIcon";
 
 const Stack = () => {
   return (
-    <section className="w-screen flex flex-col items-center">
+    <section className="w-screen flex flex-col items-center" id="stack">
       <h1 className="text-yellow-500 text-3xl">Stack</h1>
       <div className="flex flex-wrap w-[95%] mx-auto items-center justify-center">
         {stackIcons.map((icon) => (
