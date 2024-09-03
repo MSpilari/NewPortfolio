9563507f43ebb7083bb1d865fcbf3dfe28bccb3f
MSpilari <mb.spilari@hotmail.com>
2024-09-03 17:39:27 -0300

    perf: dynamic import to reduce bundle size

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
-import { Blog } from "@/src/components/Blog";
-import { Experience } from "@/src/components/Experience";
 import { Footer } from "@/src/components/Footer";
 import { Header } from "@/src/components/Header";
 import { Hero } from "@/src/components/Hero";
-import { Projects } from "@/src/components/Projects";
-import { Stack } from "@/src/components/Stack";
+import dynamic from "next/dynamic";
+
+const Blog = dynamic(() =>
+  import("@/src/components/Blog").then((mod) => mod.Blog)
+);
+const Experience = dynamic(() =>
+  import("@/src/components/Experience").then((mod) => mod.Experience)
+);
+const Projects = dynamic(() =>
+  import("@/src/components/Projects").then((mod) => mod.Projects)
+);
+const Stack = dynamic(() =>
+  import("@/src/components/Stack").then((mod) => mod.Stack)
+);
 
 export default function Home() {
   return (
