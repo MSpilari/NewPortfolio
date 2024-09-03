15085cc577650dc273521df86cf871c1ef75d003
MSpilari <mb.spilari@hotmail.com>
2024-09-03 17:11:52 -0300

    refactor: add id="projects" prop

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,7 +3,10 @@ import { ProjectCard } from "./ProjectCard";
 
 const Projects = () => {
   return (
-    <section className="w-full flex flex-col items-center justify-center">
+    <section
+      className="w-full flex flex-col items-center justify-center"
+      id="projects"
+    >
       <h1 className="text-yellow-500 text-3xl">Projects</h1>
       <div className="w-[90%] flex items-center justify-center flex-wrap gap-x-3 mx-auto">
         {projects.map((project, index) => (
