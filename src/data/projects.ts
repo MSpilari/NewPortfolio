7418f663ca17778e4eda02054735b53dcb5161cc
MSpilari <mb.spilari@hotmail.com>
2024-09-04 11:57:59 -0300

    refactor: set real projects on the file

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,11 +1,30 @@
 const projects = [
   {
-    title: " Make things float in air",
-    description: "Hover over this card to unleash the power of CSS perspective",
-    imageUrl: "/projectImage.jpg",
-    githubRepo: "",
-    url: "",
-    techs: ["/reactjs.png", "/tailwindcss.png", "/spring.png", "/mongodb.png"],
+    title: "Papa's Pizzeria",
+    description:
+      "Um aplicativo para pizzarias utilizando next, redux, tailwind e firebase",
+    imageUrl: "/papasPizzeria.png",
+    githubRepo: "https://github.com/MSpilari/papasPizzeria",
+    url: "https://papas-pizzeria.vercel.app/",
+    techs: [
+      "/reactjs.png",
+      "/tailwindcss.png",
+      "/nextjs2.png",
+      "/firebase.png",
+    ],
+  },
+  {
+    title: "French Barber",
+    description: "Uma página inicial para uma barbearia.",
+    imageUrl: "/barberShop.png",
+    githubRepo: "https://github.com/MSpilari/barber-shop",
+    url: "https://barber-shop.vercel.app/",
+    techs: [
+      "/reactjs.png",
+      "/tailwindcss.png",
+      "/nextjs2.png",
+      "/typescript.png",
+    ],
   },
 ];
 
