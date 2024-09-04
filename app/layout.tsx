e644bb9b6c508207cd6963c27c9e0887542474fc
MSpilari <mb.spilari@hotmail.com>
2024-09-04 12:43:45 -0300

    feat: insert the new icon image

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link rel="icon" href="/iconYb.png" />
       <body className={inter.className}>{children}</body>
     </html>
   );
