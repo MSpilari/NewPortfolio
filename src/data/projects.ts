dacd1377fef52910bbf0c97dcc4afaa61b4269d0
MSpilari <mb.spilari@hotmail.com>
2025-02-11 12:35:51 -0300

    feat: insert 3 projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,5 +1,35 @@
 const projects = [
   {
+    title: "Sistema emissor de Voucher",
+    description:
+      "Um sistema de geração de vouchers para uma hamburgueria, utilizando Spring Boot, Redis e Thymeleaf",
+    imageUrl: "/voucherSystem.jpg",
+    githubRepo: "https://github.com/MSpilari/voucher-system",
+    url: " ",
+    techs: [
+      "/java.png",
+      "/spring.png",
+      "/redis.png",
+      "/docker.png",
+      "/swagger.png",
+    ],
+  },
+  {
+    title: "Sistema de login(JWT) com recuperação de senha por email",
+    description:
+      "Um sistema de login com autenticação e autorização com recuperação de senha por email, utilizando Spring Boot, JWT e PostgreSQL.",
+    imageUrl: "/loginSystem.jpg",
+    githubRepo: "https://github.com/MSpilari/spring_login",
+    url: " ",
+    techs: [
+      "/java.png",
+      "/spring.png",
+      "/postgresql.png",
+      "/docker.png",
+      "/swagger.png",
+    ],
+  },
+  {
     title: "Papa's Pizzeria",
     description:
       "Um aplicativo para pizzarias utilizando next, redux, tailwind e firebase",
@@ -14,16 +44,18 @@ const projects = [
     ],
   },
   {
-    title: "French Barber",
-    description: "Uma página inicial para uma barbearia.",
-    imageUrl: "/barberShop.png",
-    githubRepo: "https://github.com/MSpilari/barber-shop",
-    url: "https://barber-shop.vercel.app/",
+    title: "Microsserviços de uma API",
+    description:
+      "Alguns microsserviços de uma API com os quais já trabalhei como proxy reverso, load balancer, rate limiter, circuit breaker, além de observabilidade e monitoramento.",
+    imageUrl: "/microservicesAPI.jpg",
+    githubRepo: "https://github.com/MSpilari/nginx-proxy-lb",
+    url: "",
     techs: [
-      "/reactjs.png",
-      "/tailwindcss.png",
-      "/nextjs2.png",
-      "/typescript.png",
+      "/java.png",
+      "/spring.png",
+      "/postgresql.png",
+      "/docker.png",
+      "/swagger.png",
     ],
   },
 ];
